b0c166d9813d17cd7107916385c914bd44e3a0f4
bcastro12 <bcastroduarte12@gmail.com>
2020-07-10 17:21:19 -0300

    updated board colors, updated ghost

--- js/app.js
+++ js/app.js
@@ -1,37 +1,45 @@
+const bgcolor = "#fff"; // default bg color
+const selectedbgcolor = "#c5cad2"; // bg color when a element is selected
 var dom;
 
-function drag(event){
-    event.target.classList.add('ghost');
-    event.dataTransfer.setData('text/plain', event.target.id);
+function drag(event) {
+    event.target.classList.add("ghost");
+    event.dataTransfer.setData("text", event.target.id);
     dom = event.target.id;
 
-    event.currentTarget.style.backgroundColor = '#bbbbbb';
+    event.currentTarget.style.backgroundColor = selectedbgcolor;
 }
 
-function drop(event){
-    const id = event.dataTransfer.getData('text');
-    console.log(id);
-
+function drop(event) {
+    const id = event.dataTransfer.getData("text");
     const draggableElement = document.getElementById(id);
-    const dropzone = event.target.closest('.items');
+    const dropzone = event.target.closest(".items");
 
-    dropzone.insertBefore(draggableElement, event.target.closest('.item'));
+    dropzone.insertBefore(draggableElement, event.target.closest(".item"));
 
-    draggableElement.style.backgroundColor = '#fff';
-    draggableElement.classList.remove('ghost');
-
+    draggableElement.style.backgroundColor = bgcolor;
+    draggableElement.classList.remove("ghost");
     event.dataTransfer.clearData();
+    check();
 }
 
-
-function over(event){
+function over(event) {
     event.preventDefault();
 
     let draggableElement = document.getElementById(dom);
-    let dropzone = event.target.closest('.items');
+    let dropzone = event.target.closest(".items");
+    dropzone.insertBefore(draggableElement, event.target.closest(".item"));
+}
 
-    //console.log(draggableElement.getAttribute('id'));
+function exit(event) {
+    document.getElementById(dom).style.backgroundColor = bgcolor;
+    document.getElementById(dom).classList.remove("ghost");
+}
 
-    //event.dataTransfer.dropEffect = 'move';
-    dropzone.insertBefore(draggableElement, event.target.closest('.item'));
+function check() {
+    elems = [...document.getElementsByClassName("items")];
+    //console.log(elems[0].children);
+    elems.forEach(() => {
+        console.log("oi");
+    });
 }
