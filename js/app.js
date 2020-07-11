bfd76e5832d1aadbcb1499dd74a5f5a83178eaf6
bruno <bcastroduarte12@gmail.com>
2020-07-11 03:03:46 -0300

    changed title's font, created add feature, created json with items position

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const bgcolor = "#fff"; // default bg color
-const selectedbgcolor = "#c5cad2"; // bg color when a element is selected
+const selectedbgcolor = "#c5cad2"; // bg color when an element is selected
 var dom;
+var obj = {};
 
 function drag(event) {
     event.target.classList.add("ghost");
@@ -20,6 +21,7 @@ function drop(event) {
     draggableElement.style.backgroundColor = bgcolor;
     draggableElement.classList.remove("ghost");
     event.dataTransfer.clearData();
+    
     check();
 }
 
@@ -29,6 +31,7 @@ function over(event) {
     let draggableElement = document.getElementById(dom);
     let dropzone = event.target.closest(".items");
     dropzone.insertBefore(draggableElement, event.target.closest(".item"));
+    
 }
 
 function exit(event) {
@@ -38,8 +41,25 @@ function exit(event) {
 
 function check() {
     elems = [...document.getElementsByClassName("items")];
-    //console.log(elems[0].children);
-    elems.forEach(() => {
-        console.log("oi");
+    elems.forEach((el,index) => {
+
+        obj[el.getAttribute('id')] = {};
+        arrEl = [];
+        [...el.children].forEach((child,i) => {
+            arrEl.push(child.getAttribute('id'));
+        });
+        obj[el.getAttribute('id')] = arrEl;
     });
+    console.log(obj);
+    document.getElementById('json').innerHTML = "<code><pre>" + JSON.stringify(obj,null,1) + "</pre></code>";
+    document.getElementById('json').style.display = 'block';
+}
+
+function sendform(){
+    let backlog = document.getElementById('backlog');
+    let newitem = document.getElementById('new-item');
+    backlog.innerHTML += "<div id='"+ (document.getElementsByClassName('item').length + 1) +"' class='item d-flex' draggable='true' ondragstart='drag(event)' ondragend='exit(event)'>"+ newitem.value +"</div>";
+    newitem.value = '';
+    check();
+    return false;
 }
